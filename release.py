108aff23139c942770f2ec3a7dd1b2389e46a938
MapleCCC <littlelittlemaple@gmail.com>
2020-06-30 16:30:00 +0800

    Turn out that Makefile has trouble escaping Chinese characters. Use standalone script instead for releasing.

---
+++ release.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import subprocess
+
+PYINSTALLER_FLAGS = (
+    '--name "基金信息生成器" --upx-dir "D:\\Apps\\upx-3.96-win64\\upx.exe" --onefile'
+)
+
+subprocess.run(
+    ["python", "-OO", "-m", "PyInstaller", PYINSTALLER_FLAGS, "main.py"]
+).check_returncode()
