808b37b1c2adfa6a10a18d2bbe311a254f6c7e96
MapleCCC <littlelittlemaple@gmail.com>
2020-06-30 16:44:20 +0800

    Fix bug when passing command line arguments to subprocess.run

--- release.py
+++ release.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 import subprocess
 
-PYINSTALLER_FLAGS = (
-    '--name "基金信息生成器" --upx-dir "D:\\Apps\\upx-3.96-win64\\upx.exe" --onefile'
-)
+PYINSTALLER_FLAGS = [
+    "--name",
+    "基金信息生成器",
+    "--upx-dir",
+    "D:\\Apps\\upx-3.96-win64\\upx.exe",
+    "--onefile",
+]
 
 subprocess.run(
-    ["python", "-OO", "-m", "PyInstaller", PYINSTALLER_FLAGS, "main.py"]
+    ["python", "-OO", "-m", "PyInstaller"] + PYINSTALLER_FLAGS + ["main.py"]
 ).check_returncode()
