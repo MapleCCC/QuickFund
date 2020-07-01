b89987b66766111cfc1c1115a6eeef09e8124aeb
MapleCCC <littlelittlemaple@gmail.com>
2020-07-01 22:27:07 +0800

    Release script also bundles the generated executable into ZIP archive.

--- scripts/release.py
+++ scripts/release.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
+from zipfile import ZipFile
 
 from main import RELEASE_ASSET_NAME, RELEASE_EXECUTABLE_NAME
+
+PYINSTALLER_DEFAULT_DISTPATH = "dist"
 
 PYINSTALLER_FLAGS = [
     "--name",
@@ -20,4 +24,5 @@ subprocess.run(
     ["python", "-OO", "-m", "PyInstaller"] + PYINSTALLER_FLAGS + ["main.py"]
 ).check_returncode()
 
-# TODO compress executable to fund-info-fetcher-win64.7z
+with ZipFile(os.path.join(PYINSTALLER_DEFAULT_DISTPATH, RELEASE_ASSET_NAME), "w") as f:
+    f.write(os.path.join(PYINSTALLER_DEFAULT_DISTPATH, RELEASE_EXECUTABLE_NAME))
