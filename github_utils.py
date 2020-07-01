412bc44405e3a155fa1e9a0db1de8cdb5ae3606f
MapleCCC <littlelittlemaple@gmail.com>
2020-07-02 00:02:13 +0800

    Refactor: Extract GitHub API query related utilities to standalone module

---
+++ github_utils.py
@@ -0,0 +1,38 @@
+import requests
+
+
+def get_latest_released_version(user: str, repo: str) -> str:
+    # TODO Handle the case when the lastest release's tag name is not semantic
+    # version.
+    response = requests.get(
+        f"https://api.github.com/repos/{user}/{repo}/releases/latest"
+    )
+    response.encoding = "utf-8"
+    json_data = response.json()
+    tag_name = json_data["tag_name"]
+    return tag_name
+
+
+def get_latest_released_asset(user: str, repo: str, asset_name: str) -> bytes:
+    print("获取最新分发版本......")
+    response = requests.get(
+        f"https://api.github.com/repos/{user}/{repo}/releases/latest"
+    )
+    response.encoding = "utf-8"
+    json_data = response.json()
+    assets = json_data["assets"]
+    candidates = list(filter(lambda asset: asset["name"] == asset_name, assets))
+    if len(candidates) == 00:
+        raise RuntimeError(
+            f"No asset with name {asset_name} can be found in the latest release"
+        )
+    elif len(candidates) > 1:
+        raise RuntimeError(
+            f"More than one assets with name {asset_name} are found in the latest release"
+        )
+    asset = candidates[0]
+    print("下载最新版本......")
+    # TODO: add display progress bar when downloading latest asset
+    return requests.get(
+        asset["url"], headers={"Accept": "application/octet-stream"}
+    ).content
--- main.py
+++ main.py
@@ -10,11 +10,11 @@ from typing import Dict, List, Tuple
 from zipfile import ZipFile
 
 import click
-import requests
 import xlsxwriter
 from tqdm import tqdm
 
 from fetcher import get_fund_info
+from github_utils import get_latest_released_asset, get_latest_released_version
 from utils import green, red
 
 __version__ = "0.2.0"
@@ -62,42 +62,6 @@ def parse_version_number(s: str) -> Tuple[int, int, int]:
     version_pattern = r"v?(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"
     major, minor, patch = re.match(version_pattern, s).group("major", "minor", "patch")
     return int(major), int(minor), int(patch)
-
-
-def get_latest_released_version(user: str, repo: str) -> str:
-    # TODO Handle the case when the lastest release's tag name is not semantic version.
-    response = requests.get(
-        f"https://api.github.com/repos/{user}/{repo}/releases/latest"
-    )
-    response.encoding = "utf-8"
-    json_data = response.json()
-    tag_name = json_data["tag_name"]
-    return tag_name
-
-
-def get_latest_released_asset(user: str, repo: str, asset_name: str) -> bytes:
-    print("获取最新分发版本......")
-    response = requests.get(
-        f"https://api.github.com/repos/{user}/{repo}/releases/latest"
-    )
-    response.encoding = "utf-8"
-    json_data = response.json()
-    assets = json_data["assets"]
-    candidates = list(filter(lambda asset: asset["name"] == asset_name, assets))
-    if len(candidates) == 00:
-        raise RuntimeError(
-            f"No asset with name {asset_name} can be found in the latest release"
-        )
-    elif len(candidates) > 1:
-        raise RuntimeError(
-            f"More than one assets with name {asset_name} are found in the latest release"
-        )
-    asset = candidates[0]
-    print("下载最新版本......")
-    # TODO: add display progress bar when downloading latest asset
-    return requests.get(
-        asset["url"], headers={"Accept": "application/octet-stream"}
-    ).content
 
 
 def write_to_xlsx(infos: List[Dict[str, str]], xlsx_filename: str) -> None:
