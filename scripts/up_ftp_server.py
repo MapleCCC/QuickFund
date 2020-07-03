29b990d339b890604685cc5e3cc5d0e36e496e03
MapleCCC <littlelittlemaple@gmail.com>
2020-07-03 15:27:51 +0800

    Add up_ftp_server.py script

---
+++ scripts/up_ftp_server.py
@@ -0,0 +1,10 @@
+import os
+import subprocess
+import sys
+
+sys.path.append(os.getcwd())
+from scripts.build import PYINSTALLER_DISTPATH
+
+subprocess.run(
+    ["python", "-m", "http.server", "--directory", PYINSTALLER_DISTPATH]
+).check_returncode()
