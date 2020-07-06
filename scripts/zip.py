2b945b47667e5a5d067efccd283c7e946bb533f9
MapleCCC <littlelittlemaple@gmail.com>
2020-07-07 00:50:57 +0800

    Fix bug in zip.py script

--- scripts/zip.py
+++ scripts/zip.py
@@ -11,9 +11,12 @@ from fetcher.__version__ import __version__
 OUTPUT_FILENAME = f"基金信息生成器 {__version__}.pyz"
 ZIPAPP_DISTPATH = "dist"
 
+if not os.path.isdir(ZIPAPP_DISTPATH):
+    os.makedirs(ZIPAPP_DISTPATH)
+
 zipapp.create_archive(
     "fetcher",
-    ZIPAPP_DISTPATH + OUTPUT_FILENAME,
+    os.path.join(ZIPAPP_DISTPATH, OUTPUT_FILENAME),
     interpreter="/usr/bin/env python3",
     compressed=True,
 )
