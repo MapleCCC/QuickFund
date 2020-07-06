f0155c11832ba89d712efca32b76b73d7fc9bcff
MapleCCC <littlelittlemaple@gmail.com>
2020-07-07 00:06:25 +0800

    Update zip.py script

--- scripts/zip.py
+++ scripts/zip.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
+import os
+import sys
 import zipapp
-# import os
 
-zipapp.create_archive("fetcher", "基金信息生成器.pyz", "/usr/bin/env python3", compressed=True)
+sys.path.append(os.getcwd())
+from fetcher.__version__ import __version__
+
+# TODO put archive to a distribution directory
+
+OUTPUT_FILENAME = f"基金信息生成器 {__version__}.pyz"
+zipapp.create_archive(
+    "fetcher", OUTPUT_FILENAME, interpreter="/usr/bin/env python3", compressed=True
+)
 
 # zipapp.create_archive(
 #     os.getcwd(),
 #     "基金信息生成器.pyz",
-#     "/usr/bin/env python3",
+#     interpreter="/usr/bin/env python3",
 #     main="fetcher.__main__:main",
 #     filter=lambda x: os.path.splitext(x)[1] == "py",
 #     compressed=True,
