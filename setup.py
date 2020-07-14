b86ac9e8602845a995c6b565bcee9d83c5b5d65b
MapleCCC <littlelittlemaple@gmail.com>
2020-07-14 23:14:01 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+from fetcher import __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Fund Info Fetcher",
+    author="MapleCCC",
+    author_email="littlelittlemaple@gmail.com",
+    description="A script to fetch various fund information from "
+    "fund.eastmoney.com, and structuralize into Excel document.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MapleCCC/Fund-Info-Fetcher",
+    version=__version__,
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=open("requirements.txt", "r").read().splitlines(),
+    entry_points={"console_scripts": ["fund-info-fetch=fetcher.__main__:main",]},
+)
