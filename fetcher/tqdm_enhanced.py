4db3b615b7ac008a44e3452e40d558d0fe773a7b
MapleCCC <littlelittlemaple@gmail.com>
2020-07-17 02:36:09 +0800

    Fix a bug: tqdm.trange does't take iterable as argument.

--- fetcher/tqdm_enhanced.py
+++ fetcher/tqdm_enhanced.py
@@ -27,12 +27,12 @@ def green_fore_color_console_wrapper(
     fn: Callable[..., Iterable]
 ) -> Callable[..., Iterator]:
     @functools.wraps(fn)
-    def wrapper(iterable: Iterable, *args: Any, **kwargs: Any) -> Iterator:
+    def wrapper(*args: Any, **kwargs: Any) -> Iterator:
         # FIXME we will lose the previous style. There is no way to restore the style
         # before the call to our function. This is problematic in the situation when
         # nesting colored console contexts are present.
         print(Fore.GREEN)
-        yield from iter(fn(iterable, *args, **kwargs))
+        yield from iter(fn(*args, **kwargs))
         print(Fore.RESET)
 
     return wrapper
