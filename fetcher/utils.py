50a419571bc59251f114b98afe354f153d69a907
MapleCCC <littlelittlemaple@gmail.com>
2020-07-16 17:05:36 +0800

    Add localization to print_traceback_digest

--- fetcher/utils.py
+++ fetcher/utils.py
@@ -1,3 +1,4 @@
+import locale
 import re
 import traceback
 from typing import Iterator, Tuple
@@ -45,9 +46,37 @@ def retrieve_succinct_traceback(tb: str) -> str:
     return "\n".join(digest)
 
 
-def print_traceback_digest(colored: bool = True) -> None:
+def print_traceback_digest(colored: bool = True, localized: bool = True) -> None:
     tb = traceback.format_exc()
     digest = retrieve_succinct_traceback(tb)
+
+    if localized:
+        cause_msg = (
+            "The above exception was the direct cause of the following exception:"
+        )
+        context_msg = (
+            "During handling of the above exception, another exception occurred:"
+        )
+        local_lang = locale.getdefaultlocale()[0]
+
+        if local_lang.startswith("en"):
+            localized_digest = digest
+        elif local_lang == "zh_CN":
+            localized_digest_lines = []
+            for line in digest.splitlines():
+                if line == context_msg:
+                    localized_digest_lines.append("在处理以上所述的错误时，发生了如下的另一个错误:")
+                elif line == cause_msg:
+                    localized_digest_lines.append("以上所述的错误直接引发了以下错误:")
+                else:
+                    line = line.replace("PermissionError", "权限错误")
+                    line = line.replace("RuntimeError", "运行时错误")
+                    localized_digest_lines.append(line)
+            localized_digest = "\n".join(localized_digest_lines)
+        else:
+            raise NotImplementedError
+
+        digest = localized_digest
 
     if colored:
         print(red(digest))
