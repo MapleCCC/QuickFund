288913950c9bd0cd9e39652540ca33dd3ae6be0f
MapleCCC <littlelittlemaple@gmail.com>
2020-08-25 00:08:13 +0800

    Add type stub for tqdm related functions

---
+++ fetcher/tqdm_enhanced.pyi
@@ -0,0 +1,16 @@
+from typing import Any, Callable, Iterable, Iterator, Tuple, TypeVar
+
+T = TypeVar("T")
+S = TypeVar("S")
+
+def tqdm(iterable: Iterable[T], *args: Any, **kwargs: Any) -> Iterator[T]: ...
+def trange(n: int, *args: Any, **kwargs: Any) -> Iterator[int]: ...
+def tmap(
+    fn: Callable[[T], S], iterable: Iterable[T], *args: Any, **kwargs: Any
+) -> Iterator[S]: ...
+def tenumerate(
+    iterable: Iterable[T], *args: Any, **kwargs: Any
+) -> Iterator[Tuple[int, T]]: ...
+def thread_map(
+    fn: Callable[[T], S], iterable: Iterable[T], *args: Any, **kwargs: Any
+) -> Iterator[S]: ...
