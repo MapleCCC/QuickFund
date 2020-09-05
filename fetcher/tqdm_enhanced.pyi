d9ac530dafc50e411eba665073dcc68bf85ff627
MapleCCC <littlelittlemaple@gmail.com>
2020-09-06 03:33:00 +0800

    Update type annotations in tqdm_enhanced.pyi

--- fetcher/tqdm_enhanced.pyi
+++ fetcher/tqdm_enhanced.pyi
@@ -1,16 +1,56 @@
-from typing import Any, Callable, Iterable, Iterator, Tuple, TypeVar
+from typing import Any, Callable, Iterable, Iterator, Tuple, TypeVar, overload
 
-T = TypeVar("T")
-S = TypeVar("S")
+_T = TypeVar("_T")
+_S = TypeVar("_S")
 
-def tqdm(iterable: Iterable[T], *args: Any, **kwargs: Any) -> Iterator[T]: ...
-def trange(n: int, *args: Any, **kwargs: Any) -> Iterator[int]: ...
+
+@overload
+def tqdm(iterable: Iterable[_T], *args: Any, **kwargs: Any) -> Iterator[_T]:
+    ...
+
+
+def tqdm(iterable: Iterable, *args: Any, **kwargs: Any) -> Iterator:
+    ...
+
+
+def trange(n: int, *args: Any, **kwargs: Any) -> Iterator[int]:
+    ...
+
+
+@overload
 def tmap(
-    fn: Callable[[T], S], iterable: Iterable[T], *args: Any, **kwargs: Any
-) -> Iterator[S]: ...
+    fn: Callable[[_T], _S], iterable: Iterable[_T], *args: Any, **kwargs: Any
+) -> Iterator[_S]:
+    ...
+
+
+def tmap(
+    fn: Callable[..., _S], iterable: Iterable, *args: Any, **kwargs: Any
+) -> Iterator[_S]:
+    ...
+
+
+@overload
 def tenumerate(
-    iterable: Iterable[T], *args: Any, **kwargs: Any
-) -> Iterator[Tuple[int, T]]: ...
+    iterable: Iterable[_T], *args: Any, **kwargs: Any
+) -> Iterator[Tuple[int, _T]]:
+    ...
+
+
+def tenumerate(
+    iterable: Iterable, *args: Any, **kwargs: Any
+) -> Iterator[Tuple[int, Any]]:
+    ...
+
+
+@overload
 def thread_map(
-    fn: Callable[[T], S], iterable: Iterable[T], *args: Any, **kwargs: Any
-) -> Iterator[S]: ...
+    fn: Callable[[_T], _S], iterable: Iterable[_T], *args: Any, **kwargs: Any
+) -> Iterator[_S]:
+    ...
+
+
+def thread_map(
+    fn: Callable[..., _S], iterable: Iterable, *args: Any, **kwargs: Any
+) -> Iterator[_S]:
+    ...
