fac23c541426c86fd400d236660ddb7dcc974997
MapleCCC <littlelittlemaple@gmail.com>
2021-07-18 05:27:08 +0800

    Update github_utils module

--- fetcher/github_utils.py
+++ fetcher/github_utils.py
@@ -1,4 +1,7 @@
 import requests
+from more_itertools import one
+from requests.packages.urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 from .utils import on_failure_raises
 
@@ -9,7 +12,19 @@ __all__ = ["get_latest_release_version", "get_latest_release_asset"]
 # Reference to GitHub official REST API document:
 # https://docs.github.com/en/rest
 
+
 ENDPOINT = "https://api.github.com"
+
+
+retry_strategy = Retry(
+    total=3,
+    backoff_factor=0.3,
+    raise_on_status=False,  # type: ignore # TODO open a PR to fix the typestub bundled with pylance
+)
+adapter = HTTPAdapter(max_retries=retry_strategy)
+sess = requests.session()
+sess.mount("https://", adapter)
+sess.mount("http://", adapter)
 
 
 @on_failure_raises(RuntimeError, "获取 {owner}/{repo} 最新发布版本时出现错误")
@@ -20,7 +35,7 @@ def get_latest_release_version(owner: str, repo: str) -> str:
 
     # According to the GitHub REST API doc, it's recommended to specify the ACCEPT key to following value in the request header.
     headers = {"accept": "application/vnd.github.v3+json"}
-    response = requests.get(
+    response = sess.get(
         f"{ENDPOINT}/repos/{owner}/{repo}/releases/latest", headers=headers
     )
     response.raise_for_status()
@@ -42,7 +57,7 @@ def download_asset(owner: str, repo: str, asset_id: str) -> bytes:
     # According to the GitHub REST API doc, it's recommended to specify the ACCEPT key to following value in the request header.
     headers = {"Accept": "application/octet-stream"}
     download_url = f"{ENDPOINT}/repos/{owner}/{repo}/releases/assets/{asset_id}"
-    r = requests.get(download_url, headers=headers,)
+    r = sess.get(download_url, headers=headers)
     r.raise_for_status()
     return r.content
 
@@ -58,23 +73,14 @@ def get_latest_release_asset(owner: str, repo: str, asset_name: str) -> bytes:
     print("获取最新分发版本......")
     # According to the GitHub REST API doc, it's recommended to specify the ACCEPT key to following value in the request header.
     headers = {"accept": "application/vnd.github.v3+json"}
-    response = requests.get(
+    response = sess.get(
         f"{ENDPOINT}/repos/{owner}/{repo}/releases/latest", headers=headers
     )
     response.raise_for_status()
     response.encoding = "utf-8"
     json_data = response.json()
     assets = json_data["assets"]
-    candidates = list(filter(lambda asset: asset["name"] == asset_name, assets))
-    if len(candidates) == 0:
-        raise RuntimeError(
-            f"No asset with name {asset_name} can be found in the latest release"
-        )
-    elif len(candidates) > 1:
-        raise RuntimeError(
-            f"More than one assets with name {asset_name} are found in the latest release"
-        )
-    that_asset = candidates[0]
-    that_asset_id = that_asset["id"]
+    asset = one(asset for asset in assets if asset["name"] == asset_name)
+    asset_id = asset["id"]
     print("下载最新版本......")
-    return download_asset(owner, repo, that_asset_id)
+    return download_asset(owner, repo, asset_id)
