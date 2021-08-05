d17796eab959ce303d2fa57dfa7e22189a89ef61
MapleCCC <littlelittlemaple@gmail.com>
2021-08-06 03:49:24 +0800

    The Typeshed [PR](https://github.com/python/typeshed/pull/5816) makes `shelve.Shelf` a generic type. No need for the hacky workaround anymore.

--- quickfund/getter.py
+++ quickfund/getter.py
@@ -5,6 +5,7 @@ from collections.abc import Iterable
 from contextlib import nullcontext
 from datetime import date, datetime, time, timedelta, timezone
 from pathlib import Path
+from shelve import Shelf
 from typing import cast
 
 from platformdirs import user_cache_dir
@@ -13,7 +14,6 @@ from .__version__ import __version__
 from .fetcher import fetch_estimate, fetch_fund_info, fetch_IARBC, fetch_net_value
 from .models import FundInfo
 from .tqdm import tqdm_asyncio
-from .typing import Shelf
 
 
 PERSISTENT_CACHE_DIR = Path(
@@ -92,25 +92,21 @@ def IARBC_date_is_latest(d: date) -> bool:
     return d == today
 
 
-async def update_estimate_info(
-    fund_code: str, fund_info_db: Shelf[str, FundInfo]
-) -> None:
+async def update_estimate_info(fund_code: str, fund_info_db: Shelf[FundInfo]) -> None:
     estimate_datetime = fund_info_db[fund_code].估算日期
     if not estimate_datetime_is_latest(estimate_datetime):
         estimate_info = await fetch_estimate(fund_code)
         fund_info_db[fund_code].replace(estimate_info=estimate_info)
 
 
-async def update_net_value_info(
-    fund_code: str, fund_info_db: Shelf[str, FundInfo]
-) -> None:
+async def update_net_value_info(fund_code: str, fund_info_db: Shelf[FundInfo]) -> None:
     net_value_date = fund_info_db[fund_code].净值日期
     if not net_value_date_is_latest(net_value_date):
         net_value_info = await fetch_net_value(fund_code)
         fund_info_db[fund_code].replace(net_value_info=net_value_info)
 
 
-async def update_IARBC_info(fund_code: str, fund_info_db: Shelf[str, FundInfo]) -> None:
+async def update_IARBC_info(fund_code: str, fund_info_db: Shelf[FundInfo]) -> None:
     IARBC_date = fund_info_db[fund_code].同类排名截止日期
     if not IARBC_date_is_latest(IARBC_date):
         IARBC_info = await fetch_IARBC(fund_code)
@@ -119,7 +115,7 @@ async def update_IARBC_info(fund_code: str, fund_info_db: Shelf[str, FundInfo]) 
 
 # TODO use a database library that supports multiple concurrent read/write
 # TODO use a database library that supports asynchronous non-blocking write
-async def update_fund_info(fund_code: str, fund_info_db: Shelf[str, FundInfo]) -> None:
+async def update_fund_info(fund_code: str, fund_info_db: Shelf[FundInfo]) -> None:
 
     if fund_code in fund_info_db:
         await update_net_value_info(fund_code, fund_info_db)
@@ -130,14 +126,12 @@ async def update_fund_info(fund_code: str, fund_info_db: Shelf[str, FundInfo]) -
         fund_info_db[fund_code] = fund_info
 
 
-def update_fund_infos(
-    fund_codes: Iterable[str], fund_info_db: Shelf[str, FundInfo]
-) -> None:
+def update_fund_infos(fund_codes: Iterable[str], fund_info_db: Shelf[FundInfo]) -> None:
     tasks = (update_fund_info(fund_code, fund_info_db) for fund_code in set(fund_codes))
     asyncio.run(tqdm_asyncio.gather(*tasks, unit="个", desc="获取基金信息"))
 
 
-def check_db_version(fund_info_db: Shelf[str, FundInfo]) -> None:
+def check_db_version(fund_info_db: Shelf[FundInfo]) -> None:
     if fund_info_db.get("version") != __version__:
         # 缓存数据库的版本不一致或版本信息缺失，清空旧存储, 并重置到当前版本
         fund_info_db.clear()
@@ -155,17 +149,15 @@ def get_fund_infos(
     FUND_INFO_CACHE_DB_PATH = PERSISTENT_CACHE_DIR / "fund-infos"
 
     if disable_cache:
-        null_shelf = cast(Shelf[str, FundInfo], {})
+        null_shelf = cast(Shelf[FundInfo], {})
         cm = nullcontext(enter_result=null_shelf)
     else:
         shelve_config = {"protocol": pickle.HIGHEST_PROTOCOL, "writeback": True}
-        cm = shelve.open(str(FUND_INFO_CACHE_DB_PATH), **shelve_config)
+        cm = cast(
+            Shelf[FundInfo], shelve.open(str(FUND_INFO_CACHE_DB_PATH), **shelve_config)
+        )
 
     with cm as fund_info_db:
-
-        # TODO wait for typeshed change to downstream to pylance
-        # TODO Add reference link to the commit when removing the cast
-        fund_info_db = cast(Shelf[str, FundInfo], fund_info_db)
 
         check_db_version(fund_info_db)
 
--- quickfund/typing.py
+++ quickfund/typing.py
@@ -1,45 +1,11 @@
-import shelve
-from collections.abc import Callable, KeysView, ValuesView
-from typing import Generic, Optional, Protocol, TypeVar, Union, overload
+from collections.abc import Callable
+from typing import Protocol, TypeVar
 
 
-__all__ = ["Shelf", "IdentityDecorator"]
+__all__ = ["IdentityDecorator"]
 
 
-T = TypeVar("T")
-K = TypeVar("K")
-V = TypeVar("V")
 FuncT = TypeVar("FuncT", bound=Callable)
-
-
-# TODO the stdlib shelve.Shelf should become a generic type, so we don't
-# need to use the following hack
-class Shelf(shelve.Shelf, Generic[K, V]):
-    def __getitem__(self, key: K) -> V:
-        ...
-
-    def __setitem__(self, key: K, value: V) -> None:
-        ...
-
-    def __delitem__(self, key: K) -> None:
-        ...
-
-    def __contains__(self, key: K) -> bool:
-        ...
-
-    @overload
-    def get(self, key: K) -> Optional[V]:
-        ...
-
-    @overload
-    def get(self, key: K, default: T) -> Union[V, T]:
-        ...
-
-    def keys(self) -> KeysView[K]:
-        ...
-
-    def values(self) -> ValuesView[K]:
-        ...
 
 
 class IdentityDecorator(Protocol):
