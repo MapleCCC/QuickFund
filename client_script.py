8193935e0528256a136f561a7e43b370a30a79a6
MapleCCC <littlelittlemaple@gmail.com>
2021-08-31 13:07:42 +0800

    [client entry script] Separate install logic and update logic.

--- client_script.py
+++ client_script.py
@@ -12,6 +12,17 @@ UPDATE_PERIOD = 4  # days
 UPDATE_TIMEOUT = 20  # seconds
 
 
+INSTALL_COMMAND = UPDATE_COMMAND = [
+    "pip",
+    "install",
+    "--compile",
+    "--upgrade-strategy",
+    "eager",
+    "--upgrade",
+    "quickfund",
+]
+
+
 def should_update() -> bool:
     return datetime.now().toordinal() % UPDATE_PERIOD == 0
 
@@ -31,26 +42,19 @@ def incognito_import(name: str) -> ModuleType:
 
 def update_quickfund() -> None:
 
+    if not should_update():
+        return
+
     print("检查更新（这个过程大概执行十至二十秒）......")
 
     old_version_info = incognito_import("quickfund").__version_info__
 
-    commands = [
-        "pip",
-        "install",
-        "--compile",
-        "--upgrade-strategy",
-        "eager",
-        "--upgrade",
-        "quickfund",
-    ]
-
     try:
         # TODO display progress bar
-        subprocess.check_call(commands, timeout=UPDATE_TIMEOUT)
+        subprocess.check_call(UPDATE_COMMAND, timeout=UPDATE_TIMEOUT)
 
     except (CalledProcessError, TimeoutError):
-        print("更新失败，请稍后再试")
+        print("更新失败，下次运行时将再次尝试更新")
 
     else:
         from quickfund import __version__, __version_info__ as new_version_info
@@ -60,12 +64,36 @@ def update_quickfund() -> None:
         elif new_version_info > old_version_info:
             print(f"更新完毕：QuickFund 库已更新至最新版本 {__version__}")
         else:
-            raise RuntimeError("出现错误：竟更新至较低版本")
+            raise RuntimeError("出现错误：竟不正确地更新至较低版本！")
+
+
+def is_quickfund_installed() -> bool:
+    try:
+        import quickfund  # type: ignore
+    except ImportError:
+        return False
+    else:
+        return True
+
+
+def install_quickfund() -> None:
+    try:
+        subprocess.check_call(UPDATE_COMMAND)
+
+    except CalledProcessError:
+        raise RuntimeError("安装 QuickFund 库失败，请重试")
+
+    else:
+        from quickfund import __version__
+
+        print(f"成功安装 QuickFund 库最新版本 {__version__}")
 
 
 def main() -> None:
 
-    if should_update():
+    if not is_quickfund_installed():
+        install_quickfund()
+    else:
         update_quickfund()
 
     # Import quickfund after it's updated
